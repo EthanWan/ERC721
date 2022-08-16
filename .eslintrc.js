dd0008c22c1a093d9b72b7ff20be75a5acd34a4d
EthanWan <wansy23@163.com>
2022-08-16 23:35:33 +0800

    feat: init yzm-doohickey

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,3 @@
 module.exports = {
-  'env': {
-    'es6': false,
-    'node': true,
-  },
-  'parserOptions': {
-    'sourceType': 'module',
-    'ecmaVersion': 8,
-  },
-  'rules': {
-    'indent': [
-      'error',
-      2,
-    ],
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'always',
-    ],
-  },
-};
+  extends: [require('yzm-doohickey').eslint],
+}
