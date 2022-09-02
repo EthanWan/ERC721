dcd772ae2d21ef2f56b624df3fe0813ccf66ba20
EthanWan <wansy23@163.com>
2022-09-02 10:54:45 +0800

    feat: react-router-v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useWeb3React } from '@web3-react/core'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
   const { connector, chainId, account, isActive } = useWeb3React()
+  const navigate = useNavigate()
 
   const connectWallet = () => {
     if (!isActive) {
@@ -19,7 +21,13 @@ function App() {
         <div className='relative flex items-center py-[2.125rem]'>
           <div className='mr-auto flex-none text-slate-900'>Logo</div>
           <div className='hidden lg:ml-8 lg:flex lg:items-center lg:border-slate-900/15 lg:pl-8'>
-            <a href='/create'>Create</a>
+            <a
+              onClick={() => {
+                navigate('/create')
+              }}
+            >
+              Create
+            </a>
             <a
               onClick={connectWallet}
               className='inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 -my-2.5 ml-8 cursor-pointer'
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core'
 import { MetaMask } from '@web3-react/metamask'
 import { hooks as metaMaskHooks, metaMask } from './connectors/metaMask'
 import './index.css'
 import App from './App'
+import Create from './Create'
 
 const connectors: [MetaMask, Web3ReactHooks][] = [[metaMask, metaMaskHooks]]
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <Web3ReactProvider connectors={connectors}>
-      <App />
-    </Web3ReactProvider>
+    <BrowserRouter>
+      <Web3ReactProvider connectors={connectors}>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/create' element={<Create />} />
+        </Routes>
+      </Web3ReactProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
