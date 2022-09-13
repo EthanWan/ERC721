505abe568cff422aa2360d51fc881de7b7f47a67
EthanWan <wansy23@163.com>
2022-09-13 22:29:26 +0800

    feat: reactive card

--- src/components/ECard.tsx
+++ src/components/ECard.tsx
@@ -5,7 +5,7 @@ interface ECardProps {
 export default function ECard(props: ECardProps) {
   const { title } = props
   return (
-    <div className='w-2/12 inline-block m-4'>
+    <div className='lg:w-2/12 md:w-4/12 sm:w-full inline-block m-4'>
       <img src={require('../assets/logo.jpg')} alt='no img' className='rounded-t-lg' />
       <div className='bg-emerald-600 h-12 rounded-b-lg'>
         <span>{title}</span>
